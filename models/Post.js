51b5a068a286ef9cca84e4904b0d0c5c9e2cbcdc
Diab <emailcodydiab@gmail.com>
2020-06-19 14:14:12 -0500

    fixed delete issue

--- models/Post.js
+++ models/Post.js
@@ -3,26 +3,26 @@ const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-class Post extends Model {
-    static upvote(body, models){
-        return models.Vote.create({
-            user_id: body.user_id,
-            post_id: body.post_id
-          }).then(() => {
-            return Post.findOne({
-              where: {
-                id: body.post_id
-              },
-              attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at'
-              ]
-            });
-          });
-    }
-}
+class Post extends Model {} //{
+//     static upvote(body, models){
+//         return models.Vote.create({
+//             user_id: body.user_id,
+//             post_id: body.post_id
+//           }).then(() => {
+//             return Post.findOne({
+//               where: {
+//                 id: body.post_id
+//               },
+//               attributes: [
+//                 'id',
+//                 'post_url',
+//                 'title',
+//                 'created_at'
+//               ]
+//             });
+//           });
+//     }
+// }
 
 
 Post.init(
